d263e086a69e6a8b856c46600ecfb0b611750d43
IsmaelCreed <razafindratsimbabrunoismael@gmail.com>
2025-03-25 02:15:48 +0300

    Second commit SideNav

--- accesbills/src/App.jsx
+++ accesbills/src/App.jsx
@@ -1,23 +1,14 @@
 import { BrowserRouter as Router} from "react-router-dom";
 import "./App.scss";
 import RoutesApplication from "./config/mainRoutes";
-import LoginPage from "./pages/auth/LoginPage";
-import MainUserPage from "./pages/user/mainUserPage";
+
 
 function App() {
   return (
     <Router>
       <RoutesApplication />
     </Router>
-    <>
-      <Router>
-        <Routes>
-          <Route path="/" element={<p>Hello</p>} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/user" element={<MainUserPage />} />
-        </Routes>
-      </Router>
-    </>
+    
   );
 }
 
